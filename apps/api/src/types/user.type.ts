ae42c815064bb09facfee1acbcdea7d41b4e373d
juliussiregar <juliussiregar1011@gmail.com>
2024-06-10 00:26:06 +0700

    feat(api): add create user request interface and edit user field interface

--- apps/api/src/types/user.type.ts
+++ apps/api/src/types/user.type.ts
@@ -1,3 +1,5 @@
+import { AccountType, UserRole, UserStatus } from "@prisma/client";
+
 export interface PublicUserResponse {
   id: string;
   displayName: string | null;
@@ -14,10 +16,23 @@ export const UserFields = {
   ...PublicUserFields,
   email: true,
   contactEmail: true,
+  displayName : true,
   role: true,
+  avatarUrl : true,
   referralCode: true,
   registerCode: true,
   status: true,
   updatedAt: true,
   createdAt: true,
+}
+
+export interface CreateUserRequest {
+  accountType : AccountType
+  email: string;
+  contactEmail :string
+  password: string;
+  displayName : string
+  avatarUrl? : string
+  role : UserRole
+  status : UserStatus
 }
