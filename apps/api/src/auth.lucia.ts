f11f544b8db71cb5f9e580555f222dc03e235a8e
rayhansoe <rayhan.rhssoe2@gmail.com>
2024-06-09 11:19:55 +0700

    feat(api): set backend logger, auth, db, validation, error handling, types, nodemailer, multer

---
+++ apps/api/src/auth.lucia.ts
@@ -0,0 +1,28 @@
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { Lucia } from "lucia";
+import { Prisma } from "@prisma/client";
+import { NODE_ENV } from "./config.js";
+import { prisma } from "./db.js";
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user)
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      secure: NODE_ENV === "production"
+    }
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      displayName: attributes.displayName,
+      role: attributes.role,
+    }
+  }
+})
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: Omit<Prisma.UserCreateInput, 'id'>
+  }
+}
