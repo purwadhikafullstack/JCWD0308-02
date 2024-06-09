1fadc48d7d6462c93e16ff9ed1e46cc609ea2fea
juliussiregar <juliussiregar1011@gmail.com>
2024-06-10 00:27:32 +0700

    feat(api): add user validation

--- apps/api/src/api/user/user.validation.ts
+++ apps/api/src/api/user/user.validation.ts
@@ -1,19 +1,33 @@
 import { ZodType, z } from 'zod';
 
 export class UserValidation {
-  static readonly REGISTER: ZodType = z.object({
+  static readonly createUser: ZodType = z.object({
+    accountType: z.enum(["EMAIL", "GOOGLE", "FACEBOOK", "GITHUB"]),
     email: z.string().email().min(11, {
+      message: 'Should be at least 11 character(s)'
+    }).max(256),
+    contactEmail: z.string().email().min(11, {
       message: 'Should be at least 11 character(s)'
     }).max(256),
     password: z.string().min(8).max(512),
     displayName: z.string().min(1).max(64).optional(),
-    registerCode: z.string().length(8).optional(),
+    avatarUrl: z.string().url().optional(),
+    role: z.enum(["SUPER_ADMIN", "STORE_ADMIN", "USER"]), 
+    status: z.enum(["ACTIVE", "INACTIVE", "SUSPENDED"]),
   })
 
-  static readonly SIGNIN: ZodType = z.object({
+  static readonly updateUser: ZodType = z.object({
+    accountType: z.enum(["EMAIL", "GOOGLE", "FACEBOOK", "GITHUB"]).optional(),
     email: z.string().email().min(11, {
       message: 'Should be at least 11 character(s)'
-    }).max(256),
-    password: z.string().min(8).max(512),
-  })
+    }).max(256).optional(),
+    contactEmail: z.string().email().min(11, {
+      message: 'Should be at least 11 character(s)'
+    }).max(256).optional(),
+    password: z.string().min(8).max(512).optional(),
+    displayName: z.string().min(1).max(64).optional(),
+    avatarUrl: z.string().url().optional(),
+    role: z.enum(["SUPER_ADMIN", "STORE_ADMIN", "USER"]).optional(),
+    status: z.enum(["ACTIVE", "INACTIVE", "SUSPENDED"]).optional(),
+  });
 }
