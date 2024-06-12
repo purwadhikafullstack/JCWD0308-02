259fd46442652d319947ac53f28e14d16c387ab1
nadirras <75488956+nadirras@users.noreply.github.com>
2024-06-12 22:17:56 +0700

    feat(api): post new stock

---
+++ apps/api/src/api/stock/stock.router.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { StockController } from './stock.controller.js';
+
+export class StockRouter {
+  private router: Router;
+  private stockController: StockController;
+
+  constructor() {
+    this.stockController = new StockController();
+    this.router = Router();
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes(): void {
+    this.router.post('/', this.stockController.addStock);
+    this.router.get('/:stockId', this.stockController.getStock);
+  }
+
+  getRouter(): Router {
+    return this.router;
+  }
+}
