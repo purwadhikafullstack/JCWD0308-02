25fa0e2eac422ced5a293eeec04b7739376190b6
rayhansoe <rayhan.rhssoe2@gmail.com>
2024-06-29 23:15:30 +0700

    feat(api): assign store admin to store while create and update

--- apps/api/src/api/user/user.service.ts
+++ apps/api/src/api/user/user.service.ts
@@ -5,59 +5,82 @@ import { UserValidation } from "./user.validation.js"
 import { ResponseError } from "@/utils/error.response.js"
 import { genSalt, hash } from "bcrypt"
 import { generateId } from "lucia"
+import { API_URL } from "@/config.js"
+import { Response } from "express"
 
 export class UserService {
-    static createUser = async (req: CreateUserRequest) => {
-        let newUser = Validation.validate(UserValidation.createUser, req)
-    
-        const findUser = await prisma.user.findUnique({
-          where: {
-            email: newUser.email,
-          }
-        })
-    
-        if (findUser) {
-          throw new ResponseError(400, "Email already used!")
-        }
-    
-        const salt = await genSalt(10)
-        const hashed = await hash(newUser.password, salt)
-    
-        const user = await prisma.user.create({
-          data: {
-            ...newUser, referralCode: generateId(8),avatarUrl: newUser.avatarUrl || "avatar.png", password: hashed
-          },
-          select: { ...UserFields }
-        })
-    
-        return user
+  static createUser = async (req: CreateUserRequest, res: Response) => {
+    let newUser = Validation.validate(UserValidation.createUser, req)
+    if (!newUser.avatarUrl) {
+      newUser.avatarUrl = `${API_URL}/public/images/avatar.png`
+    }
+
+    const findUser = await prisma.user.findUnique({
+      where: {
+        email: newUser.email,
       }
+    })
 
-      static updateUser = async (id: string, req: Partial<CreateUserRequest>) => {
-        let updatedUser = Validation.validate(UserValidation.updateUser, req);
+    if (findUser) {
+      throw new ResponseError(400, "Email already used!")
+    }
 
-        if (updatedUser.password) {
-            const salt = await genSalt(10);
-            updatedUser.password = await hash(updatedUser.password, salt);
+    const salt = await genSalt(10)
+    const hashed = await hash(newUser.password, salt)
+
+    const user = await prisma.user.create({
+      data: {
+        ...newUser, referralCode: generateId(8), avatarUrl: newUser.avatarUrl, password: hashed
+      },
+      select: { ...UserFields }
+    })
+
+    if (user.role === "STORE_ADMIN") {
+      await prisma.storeAdmin.create({
+        data: {
+          storeAdminId: user.id,
+          storeId: res.locals.store?.id!
         }
-
-        const user = await prisma.user.update({
-            where: { id },
-            data: {
-                ...updatedUser,
-            },
-            select: { ...UserFields }
-        });
-
-        return user;
+      })
     }
 
-    static deleteUser = async (id: string) => {
-        const user = await prisma.user.delete({
-            where: { id },
-            select: { ...UserFields }
-        });
+    return user
+  }
 
-        return user;
+  static updateUser = async (id: string, req: Partial<CreateUserRequest>, res: Response) => {
+    let updatedUser = Validation.validate(UserValidation.updateUser, req);
+
+    if (updatedUser.password) {
+      const salt = await genSalt(10);
+      updatedUser.password = await hash(updatedUser.password, salt);
     }
+
+    const user = await prisma.user.update({
+      where: { id },
+      data: {
+        ...updatedUser,
+      },
+      select: { ...UserFields, StoreAdmin: true }
+    });
+
+    if (user.role === "STORE_ADMIN" && user.StoreAdmin?.storeId !== res.locals.store?.id) {
+      await prisma.storeAdmin.update({
+        where: { storeAdminId: user.id },
+        data: {
+          storeId: res.locals.store?.id!
+        }
+      })
+    }
+
+    return user;
+  }
+
+  static deleteUser = async (id: string) => {
+    const user = await prisma.user.delete({
+      where: { id },
+      select: { ...UserFields }
+    });
+
+    return user;
+  }
 }
