b40b3264491340766ed23d65585b22249054541f
rayhansoe <rayhan.rhssoe2@gmail.com>
2024-07-02 08:15:26 +0700

    feat(api): add address id to res local

--- apps/api/src/custom.d.ts
+++ apps/api/src/custom.d.ts
@@ -4,6 +4,7 @@ declare global {
   namespace Express {
     interface Locals {
       user: User | null;
+      address: { id: string } | null;
       session: Session | null;
       store: { id: string } | null;
     }
