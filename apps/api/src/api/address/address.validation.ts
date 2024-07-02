734a4ed8dde69eee2e3c10c1114989c4e8aca36a
rayhansoe <rayhan.rhssoe2@gmail.com>
2024-07-02 08:25:46 +0700

    feat(api): user address API, create, read, update and delete

--- apps/api/src/api/address/address.validation.ts
+++ apps/api/src/api/address/address.validation.ts
@@ -1,10 +1,20 @@
-import { z } from 'zod';
+import { z, ZodType } from 'zod';
 
-export const AddressValidation = {
-  ADDRESS: z.object({
+export class AddressValidation  {
+  static readonly CREATE = z.object({
     userId: z.string().uuid(),
+    labelAddress: z.string(),
+    recipientName: z.string(),
+    phone: z.string(),
     address: z.string(),
-    cityId: z.number(),
+    cityId: z.string().transform(id => +id),
+    note: z.string().optional(),
     coordinate: z.string(),
-  }),
+    latitude: z.string(),
+    longitude: z.string(),
+    isMainAddress: z.boolean().default(false).optional(),
+  })
 };
+
+export type CreateAddressRequest = z.infer<typeof AddressValidation.CREATE>
+
