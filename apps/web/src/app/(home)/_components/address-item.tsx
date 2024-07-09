d8bbe60be63b68165a73a9f49c517491d29b0a47
nadirras <75488956+nadirras@users.noreply.github.com>
2024-07-09 23:16:37 +0700

    fix(web): modify address table and cartitem

--- apps/web/src/app/(home)/_components/address-item.tsx
+++ apps/web/src/app/(home)/_components/address-item.tsx
@@ -1,16 +1,11 @@
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { getSelectedAddress } from '@/lib/fetch-api/address/client';
-import { Address } from '@/lib/types/address';
-import { cn } from '@/lib/utils';
-import { useMutation, useSuspenseQuery } from '@tanstack/react-query';
-import { changeAddress } from './action';
-import { Edit, Trash } from 'lucide-react';
+import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { getSelectedAddress } from "@/lib/fetch-api/address/client";
+import { Address } from "@/lib/types/address";
+import { cn } from "@/lib/utils";
+import { useMutation, useSuspenseQuery } from "@tanstack/react-query";
+import { changeAddress } from "./action";
+import { Edit, Trash } from "lucide-react";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -21,8 +16,10 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
-import { useDeleteUserAddress } from './address-form/mutation';
+} from "@/components/ui/alert-dialog";
+import { useDeleteUserAddress } from "./address-form/mutation";
+import { useAppDispatch } from "@/lib/features/hooks";
+import { fetchCart, fetchCartItemCount } from "@/lib/features/cart/cartSlice";
 
 export default function AddressItem({
   address,
@@ -34,7 +31,7 @@ export default function AddressItem({
   setActiveTab: React.Dispatch<React.SetStateAction<string>>;
 }) {
   const { data } = useSuspenseQuery({
-    queryKey: ['selected-address'],
+    queryKey: ["selected-address"],
     queryFn: getSelectedAddress,
   });
 
@@ -49,14 +46,9 @@ export default function AddressItem({
   const handleDelete = async () => {
     await deleteAddress.mutateAsync(address.id);
   };
-
+  const dispatch = useAppDispatch();
   return (
-    <Card
-      className={cn(
-        'flex items-center justify-between',
-        address.id === data?.address?.id ? 'border-primary' : '',
-      )}
-    >
+    <Card className={cn("flex items-center justify-between", address.id === data?.address?.id ? "border-primary" : "")}>
       <CardHeader>
         <div className="flex items-center gap-2">
           <CardDescription>{address.labelAddress}</CardDescription>
@@ -65,36 +57,26 @@ export default function AddressItem({
               setAddressId(() => address.id);
               setActiveTab(() => `edit-address-${address.id}`);
             }}
-            variant={'ghost'}
-            size={'icon'}
+            variant={"ghost"}
+            size={"icon"}
             className="h-3 w-3 text-primary"
           >
             <Edit />
           </Button>
           <AlertDialog>
             <AlertDialogTrigger asChild>
-              <Button
-                variant={'ghost'}
-                size={'icon'}
-                className="h-3 w-3 text-destructive"
-              >
+              <Button variant={"ghost"} size={"icon"} className="h-3 w-3 text-destructive">
                 <Trash />
               </Button>
             </AlertDialogTrigger>
             <AlertDialogContent>
               <AlertDialogHeader>
-                <AlertDialogTitle>
-                  Are you sure want to delete {address.labelAddress} address?
-                </AlertDialogTitle>
-                <AlertDialogDescription>
-                  This action cannot be undone. This will permanently delete{' '}
-                  {address.labelAddress} from our
-                  servers.
-                </AlertDialogDescription>
+                <AlertDialogTitle>Are you sure want to delete {address.labelAddress} address?</AlertDialogTitle>
+                <AlertDialogDescription>This action cannot be undone. This will permanently delete {address.labelAddress} from our servers.</AlertDialogDescription>
               </AlertDialogHeader>
               <AlertDialogFooter>
                 <AlertDialogCancel>Cancel</AlertDialogCancel>
-                <Button variant={'destructive'} onClick={handleDelete}>
+                <Button variant={"destructive"} onClick={handleDelete}>
                   Delete
                 </Button>
               </AlertDialogFooter>
@@ -110,8 +92,10 @@ export default function AddressItem({
           <Button
             onClick={async () => {
               await handleChangeAddress.mutateAsync(address.id);
+              dispatch(fetchCart());
+              dispatch(fetchCartItemCount());
             }}
-            variant={'outline'}
+            variant={"outline"}
             className="text-primary/80 border-primary/80 hover:text-primary hover:border-primary"
           >
             Select
