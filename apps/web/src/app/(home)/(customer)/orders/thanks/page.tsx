c3457f177588ce715aa9a78b296191bf44018e6e
nadirras <75488956+nadirras@users.noreply.github.com>
2024-07-10 20:58:27 +0700

    fix(web): add thanks page and delete some unnecessary files

--- apps/web/src/app/(home)/(customer)/orders/thanks/page.tsx
+++ apps/web/src/app/(home)/(customer)/orders/thanks/page.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 export default function page() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen gap-4">
-      <h3 className="text-center">Terimakasih telah melakukan pembayaran</h3>
-      <Link href="/" className="text-center">
-        Kembali
+    <div className="flex flex-col justify-center items-center min-h-screen gap-4 bg-gray-100">
+      <h3 className="text-3xl font-bold text-center text-primary mb-4">Thank you for your purchase!</h3>
+      <Link href="/">
+        <a className="text-lg text-center text-primary hover:underline">Back to Homepage</a>
       </Link>
     </div>
   );
