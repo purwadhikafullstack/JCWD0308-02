7e012aa10ad776d88692b2f3754c58e0b6324cdb
nadirras <75488956+nadirras@users.noreply.github.com>
2024-07-12 14:03:44 +0700

    fix: fix template

--- apps/api/src/helpers/order/confirmPaymentByAdmin.ts
+++ apps/api/src/helpers/order/confirmPaymentByAdmin.ts
@@ -16,18 +16,20 @@ const __dirname = path.dirname(__filename);
 export const getOrderWithUser = async (orderId: string) => {
   return prisma.order.findUnique({
     where: { id: orderId },
-    include: { user: true },
+    include: { orderItems: { include: { stock: { include: { product: true } } } }, user: true },
   });
 };
 
 export const getEmailTemplate = (templateName: string, context: any) => {
-  const templatePath = path.join(__dirname, "../template", templateName);
+  const templatePath = path.join(__dirname, "../../template", templateName);
+  console.log("template path:", templatePath);
   const templateSource = fs.readFileSync(templatePath, "utf-8");
   const compiledTemplate = handlebars.compile(templateSource);
   return compiledTemplate(context);
 };
 
 export const sendConfirmationEmail = async (userEmail: any, subject: string, html: string) => {
+  console.log("userEmail:", userEmail);
   await transporter.sendMail({
     from: process.env.MAIL_USER,
     to: userEmail,
